58415f546a396ee112c9e96e49c5c8ab2096310f
Gaurav Kumar <gauravmunjal13@gmail.com>
2015-09-25 16:27:22 +0530

    Initial commit for indexing module. Support for two methods: index and search.

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,4 +1,6 @@
-__author__ = 'Arjun H'
+import sys
+import os
+import fnmatch
 
 '''
 This modules implements various utility like
@@ -6,14 +8,41 @@ This modules implements various utility like
  - Various string functions
 '''
 
-class Log():
+
+class Log:
     pass
 
 
-class StringHelperFunctions():
+class StringHelperFunctions:
     def stem(self):
         # TODO Implement porter stemmer or any other algorithm for stemming
         pass
     pass
+
+
+def find_files( directory, pattern='*'):
+    ''' to find files recursively in a directory '''
+    if not os.path.exists(directory):
+        raise ValueError("Directory not found {}".format(directory))
+
+    matches = []
+    for root, dirnames, filenames in os.walk(directory):
+        for filename in filenames:
+            full_path = os.path.join(root, filename)
+            if fnmatch.filter([full_path], pattern):
+                matches.append(os.path.join(root, filename))
+    return matches
+
+
+def get_function_name():
+    return sys._getframe().f_back.f_code.co_name
+
+
+def test_utils_module():
+    print(get_function_name())
+
+if __name__ == "__main__":
+    test_utils_module()
+
 
 
