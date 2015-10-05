3f892882dc93743b229164808fdce59b9ba59e27
Arjun H <harjun2090@gmail.com>
2015-10-06 00:48:31 +0530

    running application from command line only

--- src/app/main.py
+++ src/app/main.py
@@ -3,45 +3,93 @@ import os
 import src.indexingEngine.indexing_module as indexing_module
 import src.utils.utils as utils
 import datetime
-
+import argparse
 '''
 This is the main file for the Indexed Search project.
 '''
 
+
+def parse_command_line_args():
+
+    # argparse is the recommended command-line parsing module in the Python standard library.
+    parser = argparse.ArgumentParser()
+
+    # Creating a mutually exclusive group for searching and indexing
+    group = parser.add_mutually_exclusive_group()
+
+    # '-' is for short option, '--' for long option
+    # the value is stored in a variable with the name same as the long option
+    group.add_argument("-i", "--index", help="location to index")
+    group.add_argument("-s", "--search", help="query to search")
+
+    # parse the arguments
+    args = parser.parse_args()
+
+    return args
+
+def main():
+
+    # call function to parse command line arguments
+    args = parse_command_line_args()
+
+    # take action depending upon user input
+    if args.index:
+
+        # user want to index (currently support for adding index only)
+        print("Index : " + args.index)
+
+        # check whether the given directory is present or not
+        if not os.path.isdir(args.index):
+            # utils.Log.log("The directory you gave does not exist. Exiting...")
+            sys.exit("The directory you gave does not exist. Exiting...")
+
+        # get indexing module object
+        indexer = indexing_module.IndexModule()
+
+        # record start time
+        start_time = datetime.datetime.now()
+
+        # index the directory
+        indexer.index(args.index)
+
+        # record end time
+        end_time = datetime.datetime.now()
+
+        # print indexing time to the user
+        print("Indexing time := " + str(end_time - start_time))
+
+        indexer.save()
+
+    elif args.search:
+        # user wants to search a query
+        print("Search : " + args.search)
+
+        # get indexing module object
+        indexer = indexing_module.IndexModule()
+
+        # record start time
+        start_time = datetime.datetime.now()
+
+        # index the directory
+        indexer.search(args.search)
+
+        # record end time
+        end_time = datetime.datetime.now()
+
+        # print indexing time to the user
+        print("Searching time := " + str(end_time - start_time))
+
+        # split query into words
+        words = args.search.split()
+
+        # TODO Merge the results
+        # for word in words:
+        result = indexer.search(words[0])
+        utils.Log.log("Result := " + str(result))
+
+    else:
+        # TODO show usage here
+        print("No option specified")
+
 if __name__ == '__main__':
-
-    print("Welcome to indexed search.\n")
-
-    # Take input from user
-    query = input('Enter a string to search : ')
-    directory = input('Enter a folder to search this string : ')
-
-    # Check whether the given director is present or not
-    if not os.path.isdir(directory):
-        # utils.Log.log("The directory you gave does not exist. Exiting...")
-        sys.exit("The directory you gave does not exist. Exiting...")
-
-    # Create indexing module object
-    indexer = indexing_module.IndexModule()
-    # Record start time
-    start_time = datetime.datetime.now()
-
-    # Index the directory
-    indexer.index(directory)
-
-    # Record end time
-    end_time = datetime.datetime.now()
-
-    # Print indexing time to the user
-    print("Indexing time := " + str(end_time - start_time))
-
-    # split query into words
-    words = query.split()
-
-    # TODO Merge the results
-    # for word in words:
-    result = indexer.search(words[0])
-    utils.Log.log("Result := " + str(result))
-
-    # save the indexing
-    indexer.save()
+    main()
